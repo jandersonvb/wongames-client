fc1344de6a712949af9f69ff6e88735f17a98edb
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2024-10-30 15:37:58 -0300

    feat(stories): create args in stories of logo

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,13 +1,24 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { Logo } from './Logo';
+import { Meta, StoryObj } from '@storybook/react'
+import { Logo } from './Logo'
 
 export default {
   title: 'Logo',
   component: Logo,
   parameters: {
     layout: 'fullscreen'
+  },
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: ['white', 'black']
+      }
+    }
   }
-} as Meta;
+} as Meta
 
-
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  args: {
+    color: 'white'
+  }
+}
--- src/components/Logo/Logo.styles.ts
+++ src/components/Logo/Logo.styles.ts
@@ -1,3 +1,6 @@
 import styled from 'styled-components'
+import { LogoProps } from './Logo'
 
-export const Container = styled.div``
+export const Container = styled.div<LogoProps>`
+  color: ${({ theme, color }) => theme.colors[color!]};
+`
