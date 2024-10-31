c235ea0071cc735182ebe3e3525069ac8916471d
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2024-10-30 23:40:24 -0300

    test: create a test to check the label color in the logo

--- src/components/Logo/Logo.spec.tsx
+++ src/components/Logo/Logo.spec.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-
-import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from '@/utils/tests/helpers'
 import { Logo } from './Logo'
+import { screen } from '@testing-library/dom'
 
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
-    //Render the component using the 'render' function
-    // Select the element using the 'screen' object - screen.getByText('text')
-    // Expect the element  - expect(element).toBeInTheDocument()
+    renderWithTheme(<Logo />)
+    const container = screen.getByLabelText(/won games/i).parentElement
+
+    expect(container).toHaveStyle({ color: '#FAFAFA' })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    const container = screen.getByLabelText(/won games/i).parentElement
+
+    expect(container).toHaveStyle({ color: '#030517' })
   })
 })
